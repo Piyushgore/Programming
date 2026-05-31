4c0a68194540b48f19f79dcfbf1f79c396963f3c
Piyush Gore <piyushggore@gmail.com>
2026-05-31 12:58:22 +0530

    Create 03_program.c

---
+++ C_Programming/Competitive/03_program.c
@@ -0,0 +1,17 @@
+//program to print 5 to 1 numbers on screen using while loop
+#include<stdio.h>
+int Display()
+{
+    int i = 0;
+    i = 5;
+    while(i >= 1)
+    {
+        printf("%d\n", i);
+        i--;
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
