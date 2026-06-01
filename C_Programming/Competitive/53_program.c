1504be9f91d16b902bcd3ff6236c3bf2d9ce7c32
Piyush Gore <piyushggore@gmail.com>
2026-06-01 12:46:32 +0530

    Add RangeSum function to calculate range sum
    Implement a function to calculate the sum of numbers within a user-defined range, including input validation.

---
+++ C_Programming/Competitive/53_program.c
@@ -0,0 +1,55 @@
+// Write a number which accept range from user and return addition of all numbers in between that range
+
+
+#include<stdio.h>
+
+int RangeSum(int iStart,int iEnd)
+{
+    int iNum = iStart;
+    int iSum = 0;
+
+    if(iStart < 0)
+    {
+        printf("Invalid Range");
+    }
+    else
+    {
+        for(int iCnt = 0;iNum != (iEnd + 1);iCnt++)
+        {
+            if(iNum >= iStart && iNum <= iEnd)
+            {
+                iSum = iSum + iNum;    
+                iNum++;
+            } 
+        }
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    int iRet = 0;
+
+    printf("Enter starting point : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter ending point : ");
+    scanf("%d",&iValue2);
+
+    iRet = RangeSum(iValue1,iValue2);
+    if(iRet == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        printf("Addition is %d\n",iRet);
+    }
+
+    return 0;
+}
+
+// Time Complexity : O(N)
+// Where N > 0;
