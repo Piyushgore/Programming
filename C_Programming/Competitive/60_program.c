a03b707589177dc96a31c9f48e4280d62d7edd40
Piyush Gore <piyushggore@gmail.com>
2026-06-11 13:54:19 +0530

    Add program to count digits less than 6
    This program counts the frequency of digits less than 6 in a user-provided number.

---
+++ C_Programming/Competitive/60_program.c
@@ -0,0 +1,38 @@
+// Write a program which accept number from user and count frequency of such a digits which are less than 6
+
+#include<stdio.h>
+
+int Count(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit < 6)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Count(iValue);
+
+    printf("%d\n",iRet);
+
+    return 0;
+}
+
+// Time Complexity : O(N)
+// Where N >= 0
