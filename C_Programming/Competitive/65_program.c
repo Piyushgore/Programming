bd49f16490105f2e299b1dc6909fb9f298f8eeb6
Piyush Gore <piyushggore@gmail.com>
2026-06-19 10:36:34 +0530

    Implement program to display multiples of 11

---
+++ C_Programming/Competitive/65_program.c
@@ -0,0 +1,50 @@
+// Accept N numbers from user and display all such numbers which are multiples of 11
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0, iSumEven = 0, iSumOdd = 0, iDiff = 0;
+    
+    printf("Elements divisible by 5 are : \n");
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        if(((Arr[iCnt] % 11) == 0))
+        {
+            printf("%d\t",Arr[iCnt]);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iLength = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iLength);
+
+    p = (int *)malloc(iLength * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Memory Allocation failed\n");
+        return -1;
+    }
+    
+    printf("Enter %d elements : \n",iLength);
+
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p,iLength);
+
+    free(p);
+
+    return 0;
+}
