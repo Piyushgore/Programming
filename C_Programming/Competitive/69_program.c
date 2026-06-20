f5d3af40d3db62f7ef694531ef1920dc420b845e
Piyush Gore <piyushggore@gmail.com>
2026-06-20 12:52:14 +0530

    Add program to count frequency of 11 in array

---
+++ C_Programming/Competitive/69_program.c
@@ -0,0 +1,50 @@
+// Accept N numbers from user and return frequency of 11 from it
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int Arr[],int iSize)
+{
+    int iCnt = 0, iCount = 0;
+    
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            iCount++;
+        }
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iLength = 0,iCnt = 0,iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iLength);
+
+    p = (int *)malloc(iLength * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Memory Allocation failed\n");
+        return -1;
+    }
+    
+    printf("Enter %d elements : \n",iLength);
+
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Frequency(p,iLength);
+
+    printf("Result is %d\n",iRet);
+
+    free(p);
+
+    return 0;
+}
