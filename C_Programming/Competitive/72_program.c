826905b0c68920bfb1e4534dcfd56288a0909cc7
Piyush Gore <piyushggore@gmail.com>
2026-06-20 12:53:44 +0530

    Add program to find first occurrence of a number
    Implement a program to find the first occurrence of a number in an array.

---
+++ C_Programming/Competitive/72_program.c
@@ -0,0 +1,61 @@
+// Accept N numbers from user and accept one anothe number as NO, return index of first occurence of that NO
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int FirstOcc(int Arr[],int iSize, int iNo)
+{
+    int iCnt = 0, iCount = 0;
+    
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            return iCnt;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int iLength = 0,iCnt = 0,iRet = 0, iValue = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iLength);
+
+    p = (int *)malloc(iLength * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Memory Allocation failed\n");
+        return -1;
+    }
+    
+    printf("Enter %d elements : \n",iLength);
+
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    iRet = FirstOcc(p,iLength,iValue);
+
+    if(iRet == -1)
+    {
+        printf("There is no such number\n");
+    }
+    else
+    {
+        printf("The first occurence of number is %d\n",iRet);
+    }
+    
+    free(p);
+
+    return 0;
+}
