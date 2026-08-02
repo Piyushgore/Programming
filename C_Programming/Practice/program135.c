590544a536ad8cd4ed7638a3c81d6f9d19f95e03
Piyush Gore <piyushggore@gmail.com>
2026-08-02 16:12:33 +0530

    Add program to find maximum element in array
    Implement a program to find the maximum element in an array.

---
+++ C_Programming/Practice/program135.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+//Time Complexity : O(N)
+int Maximum(int Arr[],int iSize)
+{
+    int iCnt = 0;
+    int iMax = 0;
+
+    iMax = Arr[0];
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        if(Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+    }
+
+    return iMax;
+}
+
+int main()
+{
+    int iLength = 0,iRet = 0,iCnt = 0 ;
+    int *Brr = NULL;
+
+    printf("Enter the number of elements\n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elements : \n");
+
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Maximum(Brr,iLength);
+
+    printf("Maximum element is %d \n",iRet);
+
+    free(Brr);
+
+    return 0;
+}
