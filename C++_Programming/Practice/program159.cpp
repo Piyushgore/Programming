3e75f5ba7f2f578ca528eca4bc8bd363342af96b
Piyush Gore <piyushggore@gmail.com>
2026-08-07 09:46:41 +0530

    Add ArrayX class for dynamic array allocation
    Implemented a class ArrayX with a constructor and destructor for dynamic array management.

---
+++ C++_Programming/Practice/program159.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        ArrayX(int X)       // Parametrised Constructor
+        {
+            cout<<"Inside Constructor\n";
+            iSize = X;              // Characteristics initialisation
+            Arr = new int[iSize];   // Resource Allocation
+        }
+
+        // Destructor
+        ~ArrayX()
+        {
+            cout<<"Inside Destructor\n";
+
+            delete []Arr;           // Resource Deallocation
+        }
+};
+
+int main()
+{
+    // Static memory allocation for object
+    // ArrayX aobj2(5); 
+
+    ArrayX *aobj1 = new ArrayX(5);
+
+    return 0;
+}
