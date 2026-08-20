751e6490891eeeb3513c9842d0494f8928c8c219
Piyush Gore <piyushggore@gmail.com>
2026-08-20 22:25:43 +0530

    Create program351.c to show struct size calculation
    Added a C program to demonstrate struct size.

---
+++ Data_Structures/DS_in_C/program351.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node obj;
+    printf("%lu\n",sizeof(obj));        //16
+    // 4(int) + 4(padding) + 8(pointer) = 16
+
+    return 0;
+}
